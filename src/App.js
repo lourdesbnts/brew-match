9e97e5b940968374f02efb290861251ba1de32cf
Lourdes Benites <lourdesben95@gmail.com>
2022-08-03 23:26:53 -0600

    Add useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { getBeerData } from "./apiCalls";
 import Header from "./Header/Header";
 // import "./App.css"
 
 const App = () => {
   const [beers, setBeers] = useState([]);
 
-  
+    useEffect(() => {
+      getBeerData()
+      .then(data => {
+        setBeers([ ...beers, data])
+        console.log('data', data)
+      })
+    }, [])
+    console.log('beers', beers)
+
     return (
       <header>
         <Header />
