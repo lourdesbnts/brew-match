6566dfcdd53e96ac29bbb8662e28aa5657aaf832
Lourdes Benites <lourdesben95@gmail.com>
2022-08-06 16:01:58 -0600

    Add router to go to save and go back to home

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { getBeerData } from "./apiCalls";
 import BeerContainer from "./BeerContainer/BeerContainer";
 import FavoriteContainer from "./FavoriteContainer/FavoriteContainer";
 import Header from "./Header/Header";
+import { Route } from 'react-router-dom';
 // import "./App.css"
 
 const App = () => {
   const [beers, setBeers] = useState([]);
   const [favorites, setFavorites] = useState([]);
   
-
+  console.log('app', window.location.pathname)
     useEffect(() => {
       getBeerData()
       .then(data => {
@@ -23,16 +24,15 @@ const App = () => {
 
     //here
     //write new method that takes in a new favorite and runs setfavorites to add on to array on line 9 and that new method will get passed down to handle submit in beeer cards
-    console.log(favorites)
     return (
         <main>
           <Header />
-          <section>
+          <Route exact path="/">
             <BeerContainer beers={beers} saveFavorite={saveFavorite}/>
-          </section>
-          <section className="favorite">
+          </Route>
+          <Route path="/favorites" className="favorite">
             <FavoriteContainer favorites={favorites}/>
-          </section>
+          </Route>
         </main>
     )
   }
