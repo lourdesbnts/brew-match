ce18dc2d10463dfbe2456f69665fea62348edf4b
Lourdes Benites <lourdesben95@gmail.com>
2022-08-06 16:03:43 -0600

    Add favorite card to display for user selection

--- src/FavoriteCard/FavoriteCard.js
+++ src/FavoriteCard/FavoriteCard.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./FavoriteCard.css";
 
-const FavoriteCard = ({}) => {
-
+const FavoriteCard = ({beerName, beerImage, pairing}) => {
+  return (
+    <article>
+      <h3>{beerName}</h3>
+      <img src={beerImage} alt={beerName}/>
+      <p>{pairing}</p>
+    </article>
+  )
 }
 
 export default FavoriteCard;
