95c3167cfdeed7cf4fd156396bc582c41a232155
Lourdes Benites <lourdesben95@gmail.com>
2022-08-07 15:45:43 -0600

    Add data-cy to classes

--- src/BeerCard/BeerCard.js
+++ src/BeerCard/BeerCard.js
@@ -2,50 +2,62 @@ import React, { useState } from "react";
 import "./BeerCard.css";
 import PropTypes from "prop-types";
 
-
-const BeerCard = ({ beerName, tagline, abv, img, foodPairings, description, saveFavorite}) => {
+const BeerCard = ({
+  beerName,
+  tagline,
+  abv,
+  img,
+  foodPairings,
+  description,
+  saveFavorite,
+}) => {
   const [flipCards, setFlipCards] = useState(false);
   const [pairing, setPairing] = useState("");
- 
 
-const foodPair = foodPairings.map((pair) => {
-  return ( <div key={pair}>
-    <input key={pair.id} type="radio" value={pair} name="food" onChange={(event) => setPairing(event.target.value)}/> 
-    <label htmlFor={pair}>{pair}</label>
-  </div> )
-})
+  const foodPair = foodPairings.map((pair) => {
+    return (
+      <div key={pair}>
+        <input
+          key={pair.id}
+          type="radio"
+          value={pair}
+          name="food"
+          onChange={(event) => setPairing(event.target.value)}
+        />
+        <label htmlFor={pair}>{pair}</label>
+      </div>
+    );
+  });
 
-const handleSubmit = event => {
-  event.preventDefault();
-  saveFavorite({
-    pairing: pairing,
-    beerImage: img,
-    beerName: beerName
-  })
-  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    saveFavorite({
+      pairing: pairing,
+      beerImage: img,
+      beerName: beerName,
+    });
+  };
+
+  return (
+    <div className={`beer-card ${flipCards ? "flip" : ""}`} data-cy="beer-cards">
+      <div className="front" onClick={() => setFlipCards(!flipCards)} data-cy="beer-card-front">
+        <img className="image" src={img} alt={beerName} />
+        <h2>{beerName}</h2>
+        <p>{tagline}</p>
+        <p>{abv}</p>
+      </div>
+      <div className="back" data-cy="beer-card-back">
+        <p className="description" data-cy="description">{description}</p>
+        <p className="sounds-good" data-cy="sounds-good">What sounds good with this?</p>
+        <form className="food-pair" data-cy="food-pair" onSubmit={(event) => handleSubmit(event)}>
+          {foodPair}
+          <button type="submit" data-cy="save-to-try-later">SAVE TO TRY LATER</button>
+        </form>
+        <button onClick={() => setFlipCards(!flipCards)} data-cy="go-back">Go Back</button>
+      </div>
+    </div>
+  );
 };
-
-return (
-  <div className={`beer-card ${flipCards ? "flip" : ""}`}>
-  <div className="front" onClick={() => setFlipCards(!flipCards)}>
-      <img className="image" src={img} alt={beerName}/>
-      <h2>{beerName}</h2>
-      <p>{tagline}</p>
-      <p>{abv}</p>
-      
-  </div>
-  <div className="back">
-  <p>{description}</p>
-  <p>What sounds good with this?</p>
-  <form className="food-pair" onSubmit={(event) => handleSubmit(event)} >
-      {foodPair} 
-      <button type="submit">SAVE TO TRY LATER</button>
-  </form>
-      <button onClick={() => setFlipCards(!flipCards)}>Go Back</button>
-  </div>
-  </div>
-  )
-}
 
 BeerCard.propTypes = {
   beerName: PropTypes.string,
@@ -54,8 +66,7 @@ BeerCard.propTypes = {
   img: PropTypes.string,
   foodPairings: PropTypes.array,
   description: PropTypes.string,
-  saveFavorite: PropTypes.func
-}
-
+  saveFavorite: PropTypes.func,
+};
 
 export default BeerCard;
